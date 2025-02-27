66ea6fbe355561e957628ad0de204fba84f6fb71
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2025-02-27 20:38:00 +0700

    vue-facing-decorator

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 // import babel from 'vite-plugin-babel';
 // import commonjsRollup from '@rollup/plugin-commonjs';
-// import commonjsVite from 'vite-plugin-commonjs';
-// import babelConfig from './babel.config.cjs';
 
 export default defineConfig({
   plugins: [
@@ -27,47 +25,5 @@ export default defineConfig({
     // babel({
     //   babelConfig: babelConfig
     // }),
-    // // this causes app.css and bootstrap.js no default import warning
-    // commonjsVite(),
   ],
-  // resolve: {
-  //   alias: {
-  //     '@': '/resources/js',
-  //   },
-  // },
-  // // this doesn't help
-  // optimizeDeps: {
-  //   include: ['@inertiajs/vue3', 'dayjs', 'vue-facing-decorator'],
-  //   exclude: ['vue'],
-  // },
-  // // this is unnecessary
-  // build: {
-  //   outDir: 'public/build',
-  //   publicDir: 'public',
-  //   // // this causes build to not have manifest.json, error
-  //   //manifest: true,
-  //   rollupOptions: {
-  //     input: 'resources/js/app.js',  // Update the path if needed
-  //     external: [],
-  //     plugins: [
-  //       // // this causes critical import error (openBLock thing) do not use 
-  //       // commonjsRollup(), 
-  //     ],
-  //   },
-  // },
-  // // this doesn't help
-  // esbuildOptions: {
-  //   plugins: [
-  //     {
-  //       name: 'cjs-interop',
-  //       setup(build) {
-  //         build.onResolve({ filter: /.*/ }, (args) => {
-  //           if (args.path.endsWith('.cjs')) {
-  //             return { path: args.path, external: true };
-  //           }
-  //         });
-  //       },
-  //     },
-  //   ],
-  // },
 });
