3db89305c881297113afaf40e1b4b20214424b58
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2025-03-20 14:02:20 +0700

    lang ts for vue facing decorator

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import Chirp from '@/Components/Chirp.vue';
 import InputError from '@/Components/InputError.vue';
