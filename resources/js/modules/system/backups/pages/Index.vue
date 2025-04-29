12e83c70985dcaa45c5d63b48f4362934b930a7e
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2025-04-29 20:35:01 +0700

    Revert "Reapply "crud items prop and query""
    This reverts commit 41e1136c3eb4ec145a3310f3cf3317e078becaa9.

--- resources/js/modules/system/backups/pages/Index.vue
+++ resources/js/modules/system/backups/pages/Index.vue
@@ -12,7 +12,6 @@ import { ViewBase } from '@/views/ViewBase.vue';
   }
 })
 class BackupsPage extends ViewBase {
-  @Prop({ type: Array, default: null }) items;
   async mounted(){
     this.tabStore.breadcrumbs = [
       { title: this.$t('navigation.system') },
