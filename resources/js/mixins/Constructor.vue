7fd3d92f60b141ab64172b6596519e0907210323
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2025-04-30 12:18:43 +0700

    Constructor T derived from vue, but nevermind

--- resources/js/mixins/Constructor.vue
+++ resources/js/mixins/Constructor.vue
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { Vue } from 'vue-facing-decorator';
+export type ConstructorVue<T extends typeof Vue = typeof Vue> = new (...args: any[]) => T;
 export type Constructor<T = {}> = new (...args: any[]) => T;
 export default Constructor;
 </script>
