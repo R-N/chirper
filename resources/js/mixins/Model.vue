bd08f56e04cfe71c14f371b8d51812fb7354ce42
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2025-04-30 12:17:28 +0700

    model mixin

---
+++ resources/js/mixins/Model.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { Constructor } from './Constructor.vue';
+export const ModelMixin = ({ name=null, type=null, default: defaultValue=null}) => {
+  const hiddenName = name ? `_${name}` : 'value';
+  const modelName = name ? `${name}Model` : 'model';
+  name = name ?? 'modelValue';
+  return {
+    props: {
+      [name]: {
+        type: type,
+        required: false,
+      },
+    },
+    data() {
+      return {
+        [hiddenName]: defaultValue,
+      };
+    },
+    computed: {
+      [modelName]: {
+        get() {
+          if (this[name] !== undefined) {
+            return this[name];
+          }
+          return this[hiddenName];
+        },
+        set(value) {
+          this[hiddenName] = value;
+          if (this[name] !== undefined) {
+            this.$emit(`update:${name}`, value);
+          }
+        },
+      },
+    },
+  };
+}
+</script>
